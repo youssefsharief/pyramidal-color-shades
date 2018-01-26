9a9ed923e8fa187bd1a2aa0ea3fe7a21312c7be6
Youssef Sherif <sharief@aucegypt.edu>
2018-01-26 14:46:24 +0200

    Add form

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 
-
+window.onload = () => {
+    draw(20,10)
+}
 function draw(stepSize, startColor) {
-    window.onload = () => {
-        const countOfShades = 15 - startColor + 1;
-        console.log(countOfShades)
-        const colorJump = 255 / countOfShades;
-        let currentWidthNumber = 500
-        for (let i = 0; i < countOfShades; i++) {
-            const newSquare = document.createElement('div');
-            newSquare.id = `id${i}Square`;
-            const pixels = `${currentWidthNumber - ((stepSize ) * (i*2))}px`
-            newSquare.style.width = pixels
-            newSquare.style.height = pixels
-            newSquare.style.alignSelf = 'center'
-            newSquare.style.margin = 'auto'
-            newSquare.style.display = 'flex'
-            startColor = Math.floor(startColor + colorJump)
-            const newColor = `rgb(${startColor}, ${startColor}, ${startColor})`
-            newSquare.style.background = newColor
-            if (i === 0) {
-                document.querySelector(`#mainSquare`).appendChild(newSquare);
-            } else {
-                document.querySelector(`#id${i - 1}Square`).appendChild(newSquare);
-            }
+    const countOfShades = 15 - startColor + 1;
+    const colorJump = 255 / countOfShades;
+    const currentWidthNumber = countOfShades * stepSize * 2
+    for (let i = 0; i < countOfShades; i++) {
+        const newSquare = document.createElement('div');
+        newSquare.id = `id${i}Square`;
+        const pixels = `${currentWidthNumber - ((stepSize) * (i * 2))}px`
+        newSquare.style.width = pixels
+        newSquare.style.height = pixels
+        newSquare.style.alignSelf = 'center'
+        newSquare.style.margin = 'auto'
+        newSquare.style.display = 'flex'
+        startColor = Math.floor(startColor + colorJump)
+        const newColor = `rgb(${startColor}, ${startColor}, ${startColor})`
+        newSquare.style.background = newColor
+        if (i === 0) {
+            document.querySelector(`#mainSquare`).appendChild(newSquare);
+        } else {
+            document.querySelector(`#id${i - 1}Square`).appendChild(newSquare);
         }
     }
-
-
-
 }
 
+function resetGraph() {
+    document.getElementById('id0Square').remove()
+}
 
-    draw(20, 0)
+function submit() {
+    if(document.getElementById('id0Square')) {
+        resetGraph()
+    }
+    const gap = document.getElementById('gap').value
+    const firstColor = document.getElementById('first-color').value
+    if (Number.isFinite(parseFloat(gap)) && Number.isFinite(parseFloat(firstColor))) {
+        draw(parseFloat(gap), parseFloat(firstColor))
+    }
+}
